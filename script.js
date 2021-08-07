0fbb7d07e2e86f102af624e584639ef28391d60f
MAMADOU SECK <mamadouseck@Mac-mini-de-MAMADOU.local>
2021-08-07 19:38:17 +0200

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.addEventListener("touchstart", animation);
 window.addEventListener("touchmove", animation);
 
 function animation(e) {
-
     gsap.to(".cursor__circle", {
         x: e.clientX,
         y: e.clientY,
